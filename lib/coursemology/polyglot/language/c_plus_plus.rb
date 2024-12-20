ec43079862984a79a4912f0cd3cb955bd3a6b87f
adi-herwana-nus <adi.hwn@nus.edu.sg>
2024-12-20 14:10:21 +0800

    feat(cpp-versioning): add C++ 11, C++ 17 versioned concrete languages

--- lib/coursemology/polyglot/language/c_plus_plus.rb
+++ lib/coursemology/polyglot/language/c_plus_plus.rb
@@ -1,4 +1,15 @@
 class Coursemology::Polyglot::Language::CPlusPlus < Coursemology::Polyglot::Language
+  # This is the old 'C++' programming language before proper version specifiers were added.
+  # It should not be removed before all associated submissions are migrated to the versioned languages.
   syntax_highlighter 'cpp', ace: 'c_cpp'
-  concrete_language 'C/C++', docker_image: 'c_cpp'
+  concrete_language 'C/C++', docker_image: 'c_cpp:legacy'
+
+  class CPlusPlus11 < Coursemology::Polyglot::Language::CPlusPlus
+    concrete_language 'C++ 11', docker_image: 'c_cpp:11'
+  end
+
+  class CPlusPlus17 < Coursemology::Polyglot::Language::CPlusPlus
+    concrete_language 'C++ 17', docker_image: 'c_cpp:17'
+  end
+
 end
--- spec/coursemology/polyglot/language/c_plus_plus_spec.rb
+++ spec/coursemology/polyglot/language/c_plus_plus_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Coursemology::Polyglot::Language::CPlusPlus do
       expect(subject.class.display_name).to eq('C/C++')
     end
   end
+
+  describe Coursemology::Polyglot::Language::CPlusPlus::CPlusPlus11 do
+    it 'returns the correct display name' do
+      expect(subject.class.display_name).to eq('C++ 11')
+    end
+  end
+
+  describe Coursemology::Polyglot::Language::CPlusPlus::CPlusPlus17 do
+    it 'returns the correct display name' do
+      expect(subject.class.display_name).to eq('C++ 17')
+    end
+  end
 end
