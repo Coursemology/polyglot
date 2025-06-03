6475f2d3e332326d469cf5ccd7b055289f2c8de4
Nguyen Cao Duy <nguyen.cao.duy.0303@gmail.com>
2025-06-03 16:38:34 +0800

    feat(python-3.13-support): add python 3.13 with PuLP and SimPy

--- spec/coursemology/polyglot/language/python_spec.rb
+++ spec/coursemology/polyglot/language/python_spec.rb
@@ -119,4 +119,21 @@ RSpec.describe Coursemology::Polyglot::Language::Python do
       expect(subject.class.dependencies[2][:version]).to eq('3.2.1')
     end
   end
+
+  describe Coursemology::Polyglot::Language::Python::Python3Point13 do
+    it 'returns the correct display name' do
+      expect(subject.class.display_name).to eq('Python 3.13')
+    end
+
+    it 'returns the correct dependency versions' do
+      expect(subject.class.dependencies[0][:name]).to eq('fnss')
+      expect(subject.class.dependencies[0][:version]).to eq('0.9.1')
+
+      expect(subject.class.dependencies[1][:name]).to eq('matplotlib')
+      expect(subject.class.dependencies[1][:version]).to eq('3.10.3')
+
+      expect(subject.class.dependencies[2][:name]).to eq('networkx')
+      expect(subject.class.dependencies[2][:version]).to eq('3.5')
+    end
+  end
 end
